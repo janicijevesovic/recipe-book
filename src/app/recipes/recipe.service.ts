abe0fa7ae5245b7384da4ce38090775d21a172ea
janicijevesovic <janicije95@hotmail.com>
2021-11-28 14:24:19 +0100

    Using services for cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -7,27 +9,28 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe1',
+      'Tasty Schnitzel',
       'This is simply a test1',
-      'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg'
+      'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'A Test Recipe2',
+      'Big Fat Burger',
       'This is simply a test2',
-      'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe3',
-      'This is simply a test3',
-      'https://d3ur40406gizl1.cloudfront.net/web_site/images/v2/plates/4.png'
+      'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
